c87c8df7d529d4ce4cfbfc60d9c9f9a972b9d442
j3bakbak <j3bakbak@naver.com>
2023-05-22 20:20:09 +0900

    upload

---
+++ app/src/main/java/com/cookandroid/maps/MainActivity.java
@@ -0,0 +1,30 @@
+package com.cookandroid.maps;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+        Button start;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                start= findViewById(R.id.start);
+
+                start.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(), Photoactivity.class);
+                                startActivity(intent);
+                        }
+                });
+        }
+}
